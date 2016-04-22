86285eb93fb5731c3ae7f4116ec32e6dcebc760b
slanden <slanden523@gmail.com>
2016-04-21 19:05:32 -0500

    EOD Commit
    -more initial stuff
    -started graph class

--- Assets/scripts/Graph.cs
+++ Assets/scripts/Graph.cs
@@ -5,11 +5,27 @@ public class Graph : MonoBehaviour
 {
     struct Node
     {
-        int value;
+        Vector2 data;
     }
-
     struct Edge
     {
-
+        Vector2 id1, id2;
     }
+
+    Node AddNode(Vector2 data)
+    {
+        Node n = new Node();
+        
+
+        return n;
+    }
+    
+    int numNodes;
+    int maxNodes;
+
+    public Graph(int a_maxNodes, int a_numNodes = 0)
+    {
+        maxNodes = a_maxNodes;
+    }
+
 }
